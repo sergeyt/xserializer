64b7360c1088437241571c0164aad80ba8e7e17e
sergeyt <stodyshev@gmail.com>
2013-06-01 01:41:10 +0700

    comments

--- IElementDef.cs
+++ IElementDef.cs
@@ -4,29 +4,77 @@ using System.Xml.Linq;
 
 namespace XmlSerialization
 {
+	/// <summary>
+	/// Property definition.
+	/// </summary>
 	public interface IPropertyDef
 	{
+		/// <summary>
+		/// Gets property name.
+		/// </summary>
 		XName Name { get; }
+
+		/// <summary>
+		/// Gets property type.
+		/// </summary>
 		Type Type { get; }
 
+		/// <summary>
+		/// Specifies whether the property is readonly.
+		/// </summary>
 		bool IsReadOnly { get; }
 
+		/// <summary>
+		/// Gets property value.
+		/// </summary>
+		/// <param name="target">The target object to get property of.</param>
+		/// <returns></returns>
 		object GetValue(object target);
 
+		/// <summary>
+		/// Sets property value to specified object.
+		/// </summary>
+		/// <param name="target">The target object to modify.</param>
+		/// <param name="value">The value to set.</param>
 		void SetValue(object target, object value);
 	}
 
+	/// <summary>
+	/// Definition collection.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public interface IDefCollection<T> : IEnumerable<T>
 	{
+		/// <summary>
+		/// Finds definition by name.
+		/// </summary>
+		/// <param name="name">The name of definition to get.</param>
 		T this[XName name] { get; }
 	}
 
+	/// <summary>
+	/// Element definition.
+	/// </summary>
 	public interface IElementDef
 	{
+		/// <summary>
+		/// Gets element name.
+		/// </summary>
 		XName Name { get; }
+
+		/// <summary>
+		/// Gets element type.
+		/// </summary>
 		Type Type { get; }
 
+		/// <summary>
+		/// Gets element attribute definitions.
+		/// </summary>
 		IDefCollection<IPropertyDef> Attributes { get; }
+
+		/// <summary>
+		/// Gets child element definitions.
+		/// </summary>
 		IDefCollection<IPropertyDef> Elements { get; }
 	}
 }
