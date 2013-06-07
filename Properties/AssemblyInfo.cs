e63860e4a732c9e4e74f6d110747a9e8e2e4b90d
sergeyt <stodyshev@gmail.com>
2013-06-07 13:04:27 +0700

    generation of assembly version in travis build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion(ThisAssembly.Version)]
+[assembly: AssemblyFileVersion(ThisAssembly.Version)]
---
+++ Version.cs
@@ -0,0 +1,4 @@
+class ThisAssembly
+{
+	public const string Version = "0.1.0.0";
+}
