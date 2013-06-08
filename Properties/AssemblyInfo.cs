3d01153ce0cede4770a879beafe38d86df76d9fa
sergeyt <stodyshev@gmail.com>
2013-06-08 15:22:03 +0700

    fixing travis-ci build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,13 +1,20 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("XSerializer")]
+[assembly: AssemblyTitle(AssemblyInfo.Title)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TsvBits")]
-[assembly: AssemblyProduct("XSerializer")]
+[assembly: AssemblyCompany(AssemblyInfo.Company)]
+[assembly: AssemblyProduct(AssemblyInfo.Product)]
 [assembly: AssemblyCopyright("Copyright © TsvBits 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion(ThisAssembly.Version)]
-[assembly: AssemblyFileVersion(ThisAssembly.Version)]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.Version)]
+
+partial class AssemblyInfo
+{
+	public const string Title = "XSerializer";
+	public const string Company = "TsvBits";
+	public const string Product = "XSerializer";
+}
--- Version.cs
+++ Version.cs
@@ -1,4 +1,4 @@
-class ThisAssembly
+partial class AssemblyInfo
 {
 	public const string Version = "0.1.0.0";
 }
