b6f2da78a7ab1d618e99c194879b7a00f9f8adf3
sergeyt <stodyshev@gmail.com>
2013-07-08 10:21:43 +0700

    updated nuget download url

--- get-nuget.cmd
+++ get-nuget.cmd
@@ -1,4 +1,2 @@
 mkdir .nuget
-powershell -Command "(new-object System.Net.WebClient).DownloadFile('http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=nuget&DownloadId=222685&FileTime=129528596690500000&Build=20588', '.nuget\NuGet.exe')"
-.nuget\NuGet.exe
-del .nuget\NuGet.exe.old
+powershell -Command "(new-object System.Net.WebClient).DownloadFile('http://az320820.vo.msecnd.net/downloads/nuget.exe', '.nuget\NuGet.exe')"
