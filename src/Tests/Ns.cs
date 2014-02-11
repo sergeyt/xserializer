27aefd8f815f99ee8e8166a1bac1166a5168d492
sergeyt <stodyshev@gmail.com>
2014-02-11 10:27:38 +0700

    initial rdl schema

---
+++ src/Tests/Ns.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace TsvBits.Serialization.Tests
+{
+	internal static class Ns
+	{
+		public static readonly XNamespace Rdl2003 =
+			"http://schemas.microsoft.com/sqlserver/reporting/2003/10/reportdefinition";
+
+		public static readonly XNamespace Rdl2005 =
+			"http://schemas.microsoft.com/sqlserver/reporting/2005/01/reportdefinition";
+
+		public static readonly XNamespace Rdl2008 =
+			"http://schemas.microsoft.com/sqlserver/reporting/2008/01/reportdefinition";
+
+		public static readonly XNamespace Rdl2010 =
+			"http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition";
+
+		public static readonly XNamespace Dd =
+			"http://schemas.datadynamics.com/reporting/2005/02/reportdefinition";
+
+		public static readonly XNamespace Snippet =
+			"http://schemas.datadynamics.com/reporting/2005/02/snippet";
+	}
+}
---
+++ src/Tests/Rdl.cs
@@ -0,0 +1,49 @@
+﻿using TsvBits.Serialization.Tests.Rom;
+
+namespace TsvBits.Serialization.Tests
+{
+	internal static class Rdl
+	{
+		public static readonly IScope Schema;
+
+		static Rdl()
+		{
+			var schema = Scope.New(Ns.Rdl2005)
+				.Type(s => Length.Parse(s), x => x.IsValid ? x.ToString() : "")
+				.Type(s => ExpressionInfo.Parse(s), x => x.ToString())
+				.Enum(DataElementOutput.Auto);
+
+			schema.Element<Report>()
+				.Attributes()
+				.Add(x => x.Name)
+				.End()
+				.Elements()
+				.Add(x => x.Width)
+				.Add(x => x.Body);
+
+			schema.Element<Body>()
+				.Elements()
+				.Add(x => x.Height)
+				.Add(x => x.ReportItems);
+
+			var item = schema.Element<ReportItem>()
+				.Attributes()
+				.Add(x => x.Name)
+				.End()
+				.Elements()
+				.Add(x => x.DataElementName)
+				.Add(x => x.DataElementOutput)
+				.End();
+
+			item.Sub<TextBox>()
+				.Elements()
+				.Add(x => x.Value);
+
+			item.Sub<Rectangle>()
+				.Elements()
+				.Add(x => x.ReportItems);
+
+			Schema = schema;
+		}
+	}
+}
