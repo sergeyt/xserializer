440b0657b4406b38e7f601d44d8c4d32c122ac29
sergeyt <stodyshev@gmail.com>
2014-02-14 21:14:10 +0700

    test skipping of unknown elements (issue #16)

--- src/Tests/MiscTests.cs
+++ src/Tests/MiscTests.cs
@@ -5,8 +5,24 @@ using NUnit.Framework;
 namespace TsvBits.Serialization.Tests
 {
 	[TestFixture]
-	public class MultiplePropertyNamesTest
+	public class MiscTests
 	{
+		[TestCase(@"<Entity><Value>test</Value><Unknown>abc</Unknown></Entity>", Result = "test")]
+		[TestCase(@"<Entity><Unknown><nested>abc</nested></Unknown><Value>test</Value></Entity>", Result = "test")]
+		public string SkipUnknownElement(string xml)
+		{
+			var scope = new Scope();
+			scope.Element<Entity>()
+				.Elements()
+				.Add(x => x.Value)
+				.End();
+
+			var serializer = XSerializer.New(scope);
+			var obj = new Entity();
+			serializer.ReadXmlString(xml, obj);
+			return obj.Value;
+		}
+
 		[TestCase(@"<Entity><Value xmlns=""A"">test</Value></Entity>", Result = "test")]
 		[TestCase(@"<Entity><Value xmlns=""B"">test</Value></Entity>", Result = "test")]
 		[TestCase(@"<Entity><Text xmlns=""A"">test</Text></Entity>", Result = "test")]
@@ -15,7 +31,7 @@ namespace TsvBits.Serialization.Tests
 		[TestCase(@"<Entity><Text xmlns=""C"">test</Text></Entity>", Result = null)]
 		[TestCase(@"<Entity><Value>test</Value></Entity>", Result = null)]
 		[TestCase(@"<Entity><Text>test</Text></Entity>", Result = null)]
-		public string Test(string xml)
+		public string MultiplePropertyNames(string xml)
 		{
 			var ns1 = XNamespace.Get("A");
 			var ns2 = XNamespace.Get("B");
